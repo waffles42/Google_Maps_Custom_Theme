4a37c2f1e72fde961dfc534dba97dc0cad83578d
waffles42 <ivan_z@live.com>
2021-07-30 14:59:54 +0200

    Summary (required)
    Description

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+const myApiKey = "bf3e93429f89eaa838c79b663ea56ff9";
 
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
@@ -9,25 +9,23 @@ const list = document.querySelector(".ajax-section .cities");
 
 function moreInfo() {
   console.log("werks");
-  //  let li = document.getElementsByTagName("li");
-  //  for (i = 0; i < li.length; i++) {
-  //    li[i].classList.add("expanded");
-  //  }
-  popup = document.querySelector("popup");
-  popup.classList.add("add-popup");
+  let li = document.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    li[i].classList.add("expanded");
+  }
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let inputVal = input.value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${myApiKey}&units=metric`;
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(url);
-      const { main, name, sys, weather } = data;
+      const { main, wind, name, sys, weather } = data;
       const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+      console.log(url);
 
       const li = document.createElement("li");
       li.classList.add("city");
@@ -42,8 +40,14 @@ form.addEventListener("submit", (e) => {
         weather[0]["description"]
       }">
           <figcaption>${weather[0]["description"]}</figcaption></br>
-          <small onclick="moreInfo()">°°° </small>
-          
+          <small onclick="moreInfo()">°°° </small>          
+
+          <div class="expandedInfo">
+          <p>Minimum temperature: <span>${main.temp_min}</span>°C</p>          
+          <p>maximum temperature: <span>${main.temp_max}</span>°C</p>
+          <p>Pressure <span>${main.pressure} kpa/mbar</span></p>
+          <p>Windspeed <span>${wind.speed} m/s</span></p>        
+        </div>
         </figure>
       `;
       li.innerHTML = markup;
@@ -69,7 +73,9 @@ form.addEventListener("submit", (e) => {
 //responsive display ✓
 
 //change colour on hover
-//expand view on click
+//expand view on (°°°) click X
+//popup on (°°°) click
+
 //add all info to expanded view
 //add a graph
 //finish UI
